a53a3437db66dfb5aa4187bec75afd67f1025ef5
Delakroa <katon1986@gmail.com>
2021-02-09 14:41:44 +0300

    учёба

--- PySQlite.py
+++ PySQlite.py
@@ -1,12 +1,16 @@
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(path):
-    connection = None
+
+def create_connection(path):  # Определяет функцию , которая принимает путь к базе данных SQLite.
+    connection = None  # Использует метод и принимает в качестве параметра путь к базе данных SQLite.
     try:
         connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
+        print("Connection to SQLite DB successful")  # Вывод успешного подключения
     except Error as e:
-        print(f"The error '{e}' occurred")
+        print(f"The error '{e}' occurred")  # Ошибка
 
     return connection
+
+
+connection = create_connection("E:\\sm_app.sqlite")
